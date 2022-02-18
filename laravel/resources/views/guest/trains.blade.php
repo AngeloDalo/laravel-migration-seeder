3f6493fbba06cc25736a8b43d7f9b1947a596118
AngeloDalo <angelo251158@gmail.com>
2022-02-18 14:40:41 +0100

    database creato

---
+++ laravel/resources/views/guest/trains.blade.php
@@ -0,0 +1,14 @@
+
+@extends('guest.layout.base')
+
+@section('documentTitle')
+    Trains
+@endsection
+
+@section('content')
+Train
+@endsection
+
+@section('scripts')
+  <script src="{{asset('js/app.js')}}"></script>
+@endsection
